ed72ce99527c9c155fa04ccf5e6a8c12113c8410
sandra <nyamweasandra@gmail.com>
2023-05-08 00:23:07 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import BlogPost from "./BlogPost";
+import Profile from "./Profile";
 
 function App() {
-  return <BlogPost />;
+  return (
+    <div>
+      <Profile
+        name="John Smith"
+        age={30}
+        occupation="Software Developer"
+        bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula orci in nibh posuere ullamcorper. Nulla lobortis sodales elit, non commodo velit tincidunt sit amet. Praesent in interdum justo, sed varius neque."
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Profie .js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Profile(props) {
+  const { name, age, occupation, bio } = props;
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p>Occupation: {occupation}</p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
+}
+
+export default Profile;
